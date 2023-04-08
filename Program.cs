66e4ed02046863c8070ccdb084f75132699da3dc
Evgeniy Pikin <pikin@bk.ru>
2023-04-08 23:02:21 +0500

    task 8 added

--- Program.cs
+++ Program.cs
@@ -49,4 +49,21 @@
 // if (number % 2 == 0)
 //     Console.WriteLine("Число четное");
 // else
-//     Console.WriteLine("Число нечетное");           
+//     Console.WriteLine("Число нечетное");
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int evenNum = 2;
+
+// while (evenNum <= number)
+//     {
+//         Console.Write(evenNum);
+//         Console.Write(" ");
+//         evenNum += 2;
+//     }
